22263240aad73771e8afbab6425e31898e01dfa6
Arianne Acosta <adacosta3@up.edu.ph>
2024-06-14 18:32:53 +0800

    Added Arianne's Music
    Links are now album art links.

--- backend/src/main/java/org/example/backend/music/MusicConfig.java
+++ backend/src/main/java/org/example/backend/music/MusicConfig.java
@@ -19,20 +19,44 @@ public class MusicConfig {
             User arianne = userRepository.findUserByUsername("acostarianne").orElseThrow(() -> new IllegalStateException("User does not exist"));
             User joy = userRepository.findUserByUsername("joy__are").orElseThrow(() -> new IllegalStateException("User does not exist"));
 
-            Music uno = new Music(
-                    "Never Gonna Give You Up",
-                    "Whenever You Need Somebody",
-                    "Rick Astley",
-                    "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            Music a1 = new Music(
+                    "Heartbreaker",
+                    "Led Zeppelin II",
+                    "Led Zeppelin",
+                    "https://m.media-amazon.com/images/I/71mKCivU38L._SL1200_.jpg",
                     arianne);
-            Music dos = new Music(
+            Music a2 = new Music(
+                    "To Be Hunted",
+                    "Atavista",
+                    "Childish Gambino",
+                    "https://upload.wikimedia.org/wikipedia/commons/a/aa/Atavista_by_Childish_Gambino_%28album_cover%29.png",
+                    arianne);
+            Music a3 = new Music(
+                    "Casual",
+                    "The Rise and Fall of a Midwest Princess",
+                    "Chappell Roan",
+                    "https://upload.wikimedia.org/wikipedia/en/3/34/Chappell_Roan_-_The_Rise_and_Fall_of_a_Midwest_Princess.png",
+                    arianne);
+            Music a4 = new Music(
+                    "Wildflower and Barley",
+                    "Unreal Unearth: Unheard",
+                    "Hozier",
+                    "https://images.genius.com/0f9be5c00fffa18c7878cfbf75ddba87.1000x1000x1.png",
+                    arianne);
+            Music a5 = new Music(
+                    "Sundress",
+                    "Sundress - Single",
+                    "A$AP Rocky",
+                    "https://upload.wikimedia.org/wikipedia/en/e/e5/ASAP_Rocky_%E2%80%93_Sundress.png",
+                    arianne);
+            Music j1 = new Music(
                     "Karma",
                     "Karma",
                     "JoJo Siwa",
                     "https://youtu.be/VZIm_2MgdeA",
                     joy);
 
-            repository.saveAll(List.of(uno,dos));
+            repository.saveAll(List.of(a1,a2,a3,a4,a5,j1));
         };
     }
 }
